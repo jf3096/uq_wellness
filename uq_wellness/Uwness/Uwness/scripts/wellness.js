1caa0e397b01931fdf18fc5f2b8b84808abd67ec
Allen <529360897@qq.com>
2015-03-31 01:43:05 +1000

    Completed index.html and register.html animation

--- uq_wellness/Uwness/Uwness/scripts/wellness.js
+++ uq_wellness/Uwness/Uwness/scripts/wellness.js
@@ -1,6 +1,21 @@
-﻿$(function () {
-    var $circlestat = $('.circlestat').circliful();
-    console.log($circlestat);
-    $('.circlestat').on("click", function () {
-    });
+﻿
+var initCircliful = function () {
+    var $circlestat = $('.circlestat');
+    if ($circlestat.length > 0) {
+        $circlestat.context.circliful();
+        $circlestat.on("click", function () {
+        });
+    }
+};
+
+var iniWOW = function () {
+    var $wow = $(".wow");
+    if ($wow.length > 0) {
+        new WOW().init();
+    }
+}
+
+$(function () {
+    initCircliful();
+    iniWOW();
 });
