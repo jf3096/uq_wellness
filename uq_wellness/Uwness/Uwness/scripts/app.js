30a69102326a435c17cf11234b3bb66b6f54a295
Allen <allenps1991@hotmail.com>
2015-04-08 02:36:55 +0800

    integrated with ionic

---
+++ uq_wellness/Uwness/Uwness/scripts/app.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module('main', ['ionic']);
+app.controller('mainCtrl', function ($scope, $ionicSideMenuDelegate) {
+    $scope.toggleLeftSideMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+});
