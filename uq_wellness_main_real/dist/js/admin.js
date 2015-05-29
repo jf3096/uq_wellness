2c61179b7bd813c3083aa6cb865ffdfd6ea8ea8f
Hongxuan ZHU <xuanxuan0505@gmail.com>
2015-05-29 13:29:15 +1000

    report page

---
+++ uq_wellness_main_real/dist/js/admin.js
@@ -0,0 +1,9 @@
+﻿var loginValidate = function() {
+    if ($("#login-controller").length > 0) {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    }
+}();
---
+++ uq_wellness_main_real/dist/js/ng-app.js
@@ -0,0 +1,48 @@
+﻿var _url_login = "http://localhost/MvcApplication4/";
+
+var app = angular.module('AdminApp', []);
+
+app.controller('LoginController', function ($scope, loginService) {
+    $scope.login = {};
+
+    $scope.loginSubmit = function (isValid) {
+        if (isValid) {
+            loginService.login($.param($scope.login));
+        }
+    };
+    $scope.isDirtyNotNull = function ($field) {
+        return $field.$dirty && $field.$invalid;
+    };
+
+    $scope.getClassIfIsDirtyNotNull = function ($field, errClassName) {
+        return $scope.isDirtyNotNull($field) ? errClassName : "";
+    };
+});
+
+app.service("loginService", function ($http, $q) {
+    function handleError(response) {
+        if (
+            !angular.isObject(response.data) ||
+            !response.data.message
+            ) {
+            return ($q.reject("An unknown error occurred."));
+        }
+        return ($q.reject(response.data.message));
+    }
+
+    function handleSuccess(response) {
+        return (response.data);
+    }
+
+    function login(loginSerizedData) {
+        var request = $http({
+            method: "post",
+            url: _url_login,
+            data: loginSerizedData
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+    return ({
+        login: login
+    });
+});
